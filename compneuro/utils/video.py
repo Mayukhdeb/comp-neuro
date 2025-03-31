5135fc8cb2b901b998ab4e450e7990ea8a550e4d
mayukhdeb <mayukhmainak2000@gmail.com>
2025-03-31 13:04:56 -0400

    more vs stulff

--- compneuro/utils/video.py
+++ compneuro/utils/video.py
@@ -4,15 +4,18 @@ import numpy as np
 
 
 def generate_video(
-    list_of_pil_images: list, framerate: int, filename: str, size: tuple
+    list_of_pil_images: list, framerate: int, filename: str, size: tuple = None
 ):
     # Create a list to store the resized images
     resized_images = []
 
     # Iterate over each PIL image
     for pil_image in list_of_pil_images:
-        # Resize the image
-        resized_image = pil_image.resize(size, Image.NEAREST)
+        if size is not None:
+            # Resize the image
+            resized_image = pil_image.resize(size, Image.NEAREST)
+        else:
+            resized_image = pil_image
 
         # Convert the image to numpy array
         resized_array = np.array(resized_image)
@@ -22,3 +25,14 @@ def generate_video(
 
     # Save the resized images as a video using imageio
     imageio.mimwrite(filename, resized_images, fps=framerate)
+
+from IPython.display import HTML
+from base64 import b64encode
+def show_video(filename, width=200):
+    mp4 = open(filename, 'rb').read()
+    data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
+    return HTML(f"""
+    <video width="{width}" controls>
+        <source src="{data_url}" type="video/mp4">
+    </video>
+    """)
