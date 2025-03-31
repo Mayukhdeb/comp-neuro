394364d42138ffbb1cbb0abf894a33c10bb6ff1d
mayukhdeb <mayukhmainak2000@gmail.com>
2025-03-31 10:40:32 -0400

    minor refactoring

--- compneuro/datasets.py
+++ compneuro/datasets.py
@@ -1,29 +1,29 @@
 import torch
 from sklearn.model_selection import train_test_split
 
-def get_sine_wave_data(num_points: int, noise: float = 0.0, test_size: float = 0.2):
+def get_sine_wave_data(num_points: int, noise: float = 0.0, test_data_fraction: float = 0.2):
     x = torch.linspace(0, 2 * 3.14159, num_points).unsqueeze(1)  # Add batch dimension
     y = (torch.sin(x) + torch.randn_like(x) * noise)  # Add batch dimension
     
-    x_train, x_test, y_train, y_test = train_test_split(x.numpy(), y.numpy(), test_size=test_size, random_state=0)
+    x_train, x_test, y_train, y_test = train_test_split(x.numpy(), y.numpy(), test_size=test_data_fraction, random_state=0)
     
     return {
         "train": {"x": torch.tensor(x_train), "y": torch.tensor(y_train)},
         "test": {"x": torch.tensor(x_test), "y": torch.tensor(y_test)},
     }
 
-def get_line_data(num_points: int, noise: float = 0.0, test_size: float = 0.2):
+def get_line_data(num_points: int, noise: float = 0.0, test_data_fraction: float = 0.2):
     x = torch.linspace(0, 10, num_points).unsqueeze(1)  # Add batch dimension
     y = (2 * x + 1 + torch.randn_like(x) * noise)  # Add batch dimension
     
-    x_train, x_test, y_train, y_test = train_test_split(x.numpy(), y.numpy(), test_size=test_size, random_state=0)
+    x_train, x_test, y_train, y_test = train_test_split(x.numpy(), y.numpy(), test_size=test_data_fraction, random_state=0)
     
     return {
         "train": {"x": torch.tensor(x_train), "y": torch.tensor(y_train)},
         "test": {"x": torch.tensor(x_test), "y": torch.tensor(y_test)},
     }
 
-def get_zigzag_line_data(num_points: int, noise: float = 0.0, test_size: float = 0.2):
+def get_zigzag_line_data(num_points: int, noise: float = 0.0, test_data_fraction: float = 0.2):
     x = torch.linspace(-1, 1, num_points).unsqueeze(1)  # Add batch dimension
     y = (torch.abs((x * 4 % 4) - 2) * 5 + torch.randn_like(x) * noise) # Add batch dimension
     
@@ -35,7 +35,7 @@ def get_zigzag_line_data(num_points: int, noise: float = 0.0, test_size: float =
     x = x - 0.5
     y = y - 0.5
 
-    x_train, x_test, y_train, y_test = train_test_split(x.numpy(), y.numpy(), test_size=test_size, random_state=0)
+    x_train, x_test, y_train, y_test = train_test_split(x.numpy(), y.numpy(), test_size=test_data_fraction, random_state=0)
     
     return {
         "train": {"x": torch.tensor(x_train), "y": torch.tensor(y_train)},
@@ -49,6 +49,6 @@ dataset_map = {
     "zigzag_line": get_zigzag_line_data,
 }
 
-def get_dataset(name: str, num_points: int, noise: float = 0.0, test_size: float = 0.2):
+def get_dataset(name: str, num_points: int, noise: float = 0.0, test_data_fraction: float = 0.2):
     assert name in dataset_map, f"Dataset {name} not found! Please choose from {list(dataset_map.keys())}"
-    return dataset_map[name](num_points, noise, test_size)
+    return dataset_map[name](num_points, noise, test_data_fraction)
