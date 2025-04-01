33e20a08f01590f8d9b0de98157e79560611deae
Mayukh Deb <53133634+Mayukhdeb@users.noreply.github.com>
2025-03-31 23:44:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="code/material for the computational neuroscience course @ Georgia Tech",
+    name="compneuro",
     version="0.0.0",
     author="Mayukh Deb, N. Apurva Ratan Murty",
     author_email="mayukh@gatech.edu",
-    description="",
+    description="code/material for the computational neuroscience course @ Georgia Tech",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Mayukhdeb/comp-neuro",
